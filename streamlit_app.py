6af9d6d26d0a1a1ae4e57ec634b861e0d2f0ece2
John Laurenz Alvendia <88194555+JLaurenz@users.noreply.github.com>
2023-04-26 00:37:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import plotly.graph_objects as go
+import numpy as np
 
+st.set_page_config(
+    page_title="Financial Planning Calculator")
+
+st.title("Financial Planning Calculator")
